ed8d089310e6ca6ef682d96b99db142d4feebd6c
Vitor Batista <shadomal1990@gmail.com>
2020-07-22 18:51:52 -0300

    Second Commit

--- Assets/Scripts/Personagem/playerCore.cs
+++ Assets/Scripts/Personagem/playerCore.cs
@@ -9,6 +9,10 @@ public class playerCore : MonoBehaviour
 	[SerializeField] private int life;
 	[SerializeField] private int maxLife;
 	[SerializeField] private int mana;
+	[SerializeField] private GameObject OpenInventory;
+	[SerializeField] private float jumpForce = 20;
+	[SerializeField] private LayerMask groundLayers;
+	[SerializeField] private CapsuleCollider col;
 
 	private Rigidbody rbPlayer;
 	#endregion
@@ -17,6 +21,7 @@ public class playerCore : MonoBehaviour
 	void Start()
 	{
 		rbPlayer = GetComponent<Rigidbody>();
+		col = GetComponent<CapsuleCollider>();
 	}
 
 	void Update()
@@ -24,6 +29,11 @@ public class playerCore : MonoBehaviour
 		float moveH = Input.GetAxis("Horizontal");
 		float moveV = Input.GetAxis("Vertical");
 		move(moveH, moveV);
+
+		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+			Pulo();
+        }
 	}
 
 	#region Movimentacao
@@ -31,19 +41,39 @@ public class playerCore : MonoBehaviour
 	public void move(float moveH, float moveV)
 	{
 		Vector3 novaVel = Vector3.right * moveH * speed;
-		if (moveH < 0 && transform.rotation.eulerAngles.y != 270)
+		if (moveH < 0 && transform.rotation.eulerAngles.y != 180)
 		{
-			Vector3 newRot = new Vector3(0, 270, 0);
+			Vector3 newRot = new Vector3(0, 180, 0);
 			transform.rotation = Quaternion.Euler(newRot);
 		}
-		if (moveH > 0 && transform.rotation.eulerAngles.y != 90)
+		if (moveH > 0 && transform.rotation.eulerAngles.y != 0)
 		{
-			Vector3 newRot = new Vector3(0, 90, 0);
+			Vector3 newRot = new Vector3(0, 0, 0);
 			transform.rotation = Quaternion.Euler(newRot);
 		}
 
 		novaVel.y = rbPlayer.velocity.y;
 		rbPlayer.velocity = novaVel;
 	}
+    #endregion
+
+    #region Pulo
+	void Pulo()
+    {
+		rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+	private bool IsGrounded()
+    {
+		return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x,
+			col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+
+    #endregion
+    #region Metodos
+    public void InventoryOpen(){
+		
+	}
 	#endregion
-}
+
+}	
